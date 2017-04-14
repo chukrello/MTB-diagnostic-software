2f106a300b91a15017b011637b8dc3bbd439380a
chukrello <chukreev55@gmail.com>
2017-04-14 13:58:59 +0300

    В покрытие добавился апстрим

--- 2_coverage/extract_depth.py
+++ 2_coverage/extract_depth.py
@@ -10,12 +10,33 @@ import os
 list_genes = ['ahpC', 'eis', 'embA', 'embB', 'embC', 'embR', 'fabG1', 'gid', 'gyrA', 'gyrB', 'inhA', 'iniA', 'iniC',
              'katG', 'nat' 'ndh', 'pncA', 'rpoB', 'rpsA', 'rpsL', 'rrs', 'tlyA']
 all_ids = [line[:-1] for line in open('../1_input/subsets/Full_subset.txt').readlines()]
-trace = calculate_trace(list_genes)
 os.system('mkdir coverage_output')
 
 
+def get_genes_info():
+    import re
+    lines = open('../1_input/AL123456_rev.gff').readlines()
+    gene_reg = re.compile('\tgene \w*')
+    genes = []
+    for line in lines:
+        m = gene_reg.search(line)
+        if m:
+            genes.append(line)
+    genes = [[line[:-1].split('\t')[3],line[:-1].split('\t')[4], line[:-1].split('\t')[8].split(' ')[1], line[:-1].split('\t')[6]] for line in genes]
+    list_genes = ['ahpC', 'eis', 'embA', 'embB', 'embC', 'embR', 'fabG1', 'gid', 'gyrA', 'gyrB', 'inhA', 'iniA', 'iniC',
+             'katG', 'manB', 'ndh', 'pncA', 'rmlD', 'rpoB', 'rpsA', 'rpsL', 'rrs', 'tlyA']
+    def get_sequence(genes, gene):
+        for element in genes:
+            if element[2] == gene:
+                return [int(element[0]), int(element[1]), element[3]]
+    genes_seqs = dict()
+    for gene in list_genes:
+        genes_seqs[gene] = get_sequence(genes,gene)
+
+    return genes_seqs
+
 # trace
-def calculate_trace(list_genes):
+def calculate_trace(list_genes, genes):
 
     lines = [[int(line.split('\t')[3]),int(line.split('\t')[4]), line] for line in open('../1_input/AL123456_rev.gff').readlines()]
 
@@ -24,8 +45,12 @@ def calculate_trace(list_genes):
     for gene in list_genes:
         for line in lines:
             if gene in line[2]:
-                trace.append([line[0], line[1]])
-                break
+                if genes[gene][2] == '+':
+                    trace.append([line[0]-150, line[1]])
+                    break
+                else:
+                    trace.append([line[0], line[1] + 150])
+                    break
 
     return trace
 
@@ -64,6 +89,7 @@ def extract_coverage(id, trace):
 
     file.close()
 
-
+genes = get_genes_info()
+trace = calculate_trace(list_genes, genes)
 from joblib import Parallel, delayed
 Parallel(n_jobs=-1)(delayed(extract_coverage)(id, trace) for id in all_ids)
